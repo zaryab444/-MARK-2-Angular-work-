5122a9baccc436e6ac6ff28eb7950db0d527234d
Zaryab <sohail.zaryab61@gmail.com>
2020-04-21 01:36:16 -0700

    angular service

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish'
-import { DISHES } from '../shared/dishes';
+import { DishService } from '../services/dish.service';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -8,15 +8,20 @@ import { DISHES } from '../shared/dishes';
 })
 export class MenuComponent implements OnInit {
 
-  dishes: Dish[] = DISHES;
+  dishes: Dish[];
+  
   selectedDish: Dish;
   //selectedDish = DISHES[0];
 
-  constructor() { }
+  constructor(private dishService: DishService) { }
 
   ngOnInit() {
+   // this.dishes = this.dishService.getDishes();
+    this.dishes = this.dishService.getDishes();
   }
+
   onSelect(dish: Dish) {
     this.selectedDish = dish;
   }
 }
+ 
--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,3 +1,4 @@
+import {Comment} from './comment';
 export class Dish {
     id: string;
     name: string;
@@ -7,15 +8,6 @@ export class Dish {
     label: string;
     price: string;
     description: string;
+    comments:Comment[];
 }
 
-export class Dishdetail {
-    id: string;
-    name: string;
-    image: string;
-    category: string;
-    featured: boolean;
-    label: string; 
-    price: string;
-    description: string;
-}
